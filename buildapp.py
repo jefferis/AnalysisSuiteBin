c3a979d26eaab9ebcf7b5fcfca8f572a1dca829f
Gregory Jefferis <jefferis@gmail.com>
2010-10-06 21:44:46 +0100

    Documentation in buildapp

--- buildapp.py
+++ buildapp.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+
+# Script to build a MacOS X Application from callmungereasygui.py
+# Just run:
+# python buildapp.py build
+
 from bundlebuilder import buildapp
 
 buildapp(

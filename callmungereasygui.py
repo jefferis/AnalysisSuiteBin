5d0d4c43676f1e89b9eba090f50f0c521fbbf2c1
Gregory Jefferis <jefferis@gmail.com>
2010-10-06 15:48:18 +0100

    Remove wild card import of easygui

--- callmungereasygui.py
+++ callmungereasygui.py
@@ -4,61 +4,61 @@
 # script which coordinates registration and reformatting of images using CMTK
 
 import os, sys, time
-from easygui import *
+import easygui
 from subprocess import *
 
 # Function definitions
 # --------------------
 def relpath(path, basedir = os.curdir):
-	''' @return a relative path from basedir to (sub) path.
-	
-	basedir defaults to curdir
-	Does not work with paths outside basedir
-	This is superseded by a builtin in python >=2.6
-	'''
-	basedir = os.path.abspath(basedir)
-	path = os.path.abspath(path)
-	
-	if basedir == path :
-		return '.'
-	if basedir in path :
-		return path.replace(basedir+'/','')
-	return (path)
+    ''' @return a relative path from basedir to (sub) path.
+    
+    basedir defaults to curdir
+    Does not work with paths outside basedir
+    This is superseded by a builtin in python >=2.6
+    '''
+    basedir = os.path.abspath(basedir)
+    path = os.path.abspath(path)
+    
+    if basedir == path :
+        return '.'
+    if basedir in path :
+        return path.replace(basedir+'/','')
+    return (path)
 
 def makescript(cmd,rootdir,outdir):
-	'''
-	Make a shell script file that can be used to rerun the warp action
-	
-	Because the file is executable and ends in .command it can be 
-	double clicked on MacOS X
-	'''
-	if not os.path.exists: os.mkdir(outdir)
-	mtime = time.strftime('"%Y-%m-%d_%H.%M.%S"')
-	filename= "munger_%s.command" %(mtime)
-	filepath=os.path.join(outdir,filename)
-	f = open(filepath, 'w')
-	f.write('#!/bin/sh\n# %s\ncd \"%s"\n%s' % (mtime, rootdir, cmd))
-	os.chmod(filepath,0755)
-	return filepath
+    '''
+    Make a shell script file that can be used to rerun the warp action
+    
+    Because the file is executable and ends in .command it can be 
+    double clicked on MacOS X
+    '''
+    if not os.path.exists: os.mkdir(outdir)
+    mtime = time.strftime('"%Y-%m-%d_%H.%M.%S"')
+    filename= "munger_%s.command" %(mtime)
+    filepath=os.path.join(outdir,filename)
+    f = open(filepath, 'w')
+    f.write('#!/bin/sh\n# %s\ncd \"%s"\n%s' % (mtime, rootdir, cmd))
+    os.chmod(filepath,0755)
+    return filepath
 
 def findExecutable(execname,msg=''):
-	'''
-	@return full path to a command using the shell's which function
-	'''
-	execpath=Popen(["which", execname], stdout=PIPE).communicate()[0].rstrip()
-	if execpath == '' :
-		if msg == '' :
-			msg="Locate the program "+execname
-		newpath = fileopenbox(msg=msg)
-		return newpath
-	else : return execpath
+    '''
+    @return full path to a command using the shell's which function
+    '''
+    execpath=Popen(["which", execname], stdout=PIPE).communicate()[0].rstrip()
+    if execpath == '' :
+        if msg == '' :
+            msg="Locate the program "+execname
+        newpath = easygui.fileopenbox(msg=msg)
+        return newpath
+    else : return execpath
 
 # # the script !
 # # --------------------
 
 scriptPath = os.path.join(sys.path[0], sys.argv[0])
 print scriptPath
-textbox(msg='scriptPath = %s' % (scriptPath), title=' ', text='', codebox=0)
+easygui.textbox(msg='scriptPath = %s' % (scriptPath), title=' ', text='', codebox=0)
 
 # 0.1) Identify path to CMTK binaries
 # see if warp is in path
@@ -69,29 +69,29 @@ bindir=os.path.dirname(findExecutable('warp'))
 munger=findExecutable('munger.pl')
 
 # 1) Select input directory/files
-image=diropenbox("Choose the source images directory", 
-	"Select Input Directory")
+image=easygui.diropenbox("Choose the source images directory", 
+    "Select Input Directory")
 if image == None:
-	sys.exit("No image chosen\n")
+    sys.exit("No image chosen\n")
 # if os.path.basename(image) != "images":
-# 	sys.exit("Images directory must be called images\n")
+#     sys.exit("Images directory must be called images\n")
 
 rootDir=os.path.dirname(image)
-print "New working directory is: "+rootDir	
+print "New working directory is: "+rootDir    
 os.chdir(rootDir)
 
 # 2) Choose the reference brain
-refBrain = fileopenbox("Choose the reference brain: this can be a PIC or a nrrd file","Select Reference Brain")
+refBrain = easygui.fileopenbox("Choose the reference brain: this can be a PIC or a nrrd file","Select Reference Brain")
 print refBrain
 refBrain=relpath(refBrain,rootDir)
 print refBrain
 
 # 3) Select additional flags
-mungeropts=enterbox(msg='Please enter the options to be passed to munger', default='-a -w -r 01 -v', strip=True)
+mungeropts=easygui.enterbox(msg='Please enter the options to be passed to munger', default='-a -w -r 01 -v', strip=True)
 
 # multchoicebox(msg='What actions do you want', title=' ', choices=('affine','warp','reformat'))
 # 4) Action
-action=buttonbox(msg='Choose an action', choices=('Test', 'Run', 'Write script','Cancel'))
+action=easygui.buttonbox(msg='Choose an action', choices=('Test', 'Run', 'Write script','Cancel'))
 if action == 'Cancel': sys.exit('script cancelled!')
 if action == 'Test': mungeropts+=' -t'
 # build munger command line
@@ -101,7 +101,7 @@ print cmd
 script=makescript(cmd,rootDir,outdir=os.path.join(rootDir,'commands'))
 
 if action != 'Write script':
-	# Actually run the script	
-	call(script,shell=True)
+    # Actually run the script    
+    call(script,shell=True)
 
 print ("Successfully completed!")

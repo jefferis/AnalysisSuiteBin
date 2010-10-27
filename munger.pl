jefferis/AnalysisSuiteBin

commit 03213fad521acf83fdb17733d902cf19d159310b

munger: allow warp suffix to contain dots
* fixes a problem with reformatted image names when energy weight set to 0.5

munger.pl
@@ -432,7 +432,7 @@ sub runReformat {
 	# Construct the outlist - basename gets the name of the file from full path
 	my $outlist=basename($baseinlist);  #eg averagegoodbrain_brainame_warp40-5_e1e-1_c4.list
 	# Remove everything up to warp or 9dof
-	$outlist=~s/^.*_((warp|9dof|pa)[^.]*)\.list$/$1/i;
+	$outlist=~s/^.*_((warp|9dof|pa)^.*?)\.list$/$1/i;
 	# nb registration channel may be different from channel of current image
 	# which is what we want here		
 	if( $level=~m/[0-9]/ ){

commit 36b3000188bec3cd6bde2d1458d3c7d01092c84e

Fix duplication of stem names in reformatted output files
* fixes regex that was broken by 03213fad521acf83fdb17733d902cf19d159310b
* the regex was never matching because of a spurious start of line (^)
  symbol that was left over
* ? is also irrelevant because .list anchors search

munger.pl
@@ -454,7 +454,7 @@ sub runReformat {
 	# Construct the outlist - basename gets the name of the file from full path
 	my $outlist=basename($baseinlist);  #eg averagegoodbrain_brainame_warp40-5_e1e-1_c4.list
 	# Remove everything up to warp or 9dof
-	$outlist=~s/^.*_((warp|9dof|pa)^.*?)\.list$/$1/i;
+	$outlist=~s/^.*_((warp|9dof|pa).*)\.list$/$1/i;
 	# nb registration channel may be different from channel of current image
 	# which is what we want here
 	if( $level=~m/[0-9]/ ){